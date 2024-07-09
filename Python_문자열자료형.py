5fd519a3155a90876c607916eeae5ff07d3abaa1
Guitarfish <ayrcap@gmail.com>
2024-07-09 16:46:00 +0900

    문자열 자료형

---
+++ Python_문자열자료형.py
@@ -0,0 +1,14 @@
+########### 문자열 자료형 ###########
+
+#문자열 초기화
+str = 'Hello World'
+print(str)
+str = "'Hello World'"
+print(str)
+str = "Hello \"World\""
+print(str)
+
+
+#문자열 연산
+print(str*2)
+print(str[0:6])
