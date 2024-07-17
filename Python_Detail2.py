299d652c131922a8b2f03c423935672ace24717c
Guitarfish <ayrcap@gmail.com>
2024-07-17 17:09:49 +0900

    여러값 입력

--- Python_Detail2.py
+++ Python_Detail2.py
@@ -3,3 +3,11 @@
 
 
 # 비트 연산자:  ~(bitwise not), &(bitwise and), |(bitwise or), ^(bitwise xor)
+
+''' 
+    줄바꿈으로 임의의 여러값 받기
+    import sys
+    data = sys.stdin.read().strip()
+    list = list(map(int,data.split()))
+'''
+
